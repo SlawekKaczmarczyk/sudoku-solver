25deea82359d45d4540dd15eb92990bcc283a0b7
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-11-10 14:34:07 +0100

    Change of sudoku grid - adding resolveable

--- Main.hs
+++ Main.hs
@@ -1,25 +1,31 @@
-type Sudoku = [[Int]]
+type Sudoku = [[Maybe Int]]
+
+size :: Int
+size = 9
 
 main :: IO ()
 main = do
   putStrLn "Original Sudoku:"
   printSudoku exampleSudoku
 
-exampleSudoku :: Sudoku
+exampleSudoku :: Sudoku -- Example Sudoku grid
 exampleSudoku =
-  [ [5, 3, 0, 0, 7, 0, 0, 0, 0]
-  , [6, 0, 0, 1, 9, 5, 0, 0, 0]
-  , [0, 9, 8, 0, 0, 0, 0, 6, 0]
-  , [8, 0, 0, 0, 6, 0, 0, 0, 3]
-  , [4, 0, 0, 8, 0, 3, 0, 0, 1]
-  , [7, 0, 0, 0, 2, 0, 0, 0, 6]
-  , [0, 6, 0, 0, 0, 0, 2, 8, 0]
-  , [0, 0, 0, 4, 1, 9, 0, 0, 5]
-  , [0, 0, 0, 0, 8, 0, 0, 7, 9]
-  ]
+    [ [Just 5, Nothing, Nothing, Just 8, Nothing, Nothing, Nothing, Just 4, Just 9]
+    , [Nothing, Just 9, Nothing, Just 7, Nothing, Just 1, Nothing, Just 8, Nothing]
+    , [Nothing, Nothing, Just 3, Nothing, Just 5, Nothing, Just 6, Nothing, Nothing]
+    , [Just 6, Nothing, Just 8, Nothing, Nothing, Nothing, Just 3, Nothing, Just 7]
+    , [Nothing, Just 4, Nothing, Nothing, Nothing, Nothing, Nothing, Just 1, Nothing]
+    , [Just 2, Nothing, Just 7, Nothing, Nothing, Nothing, Just 9, Nothing, Just 8]
+    , [Nothing, Nothing, Just 1, Nothing, Just 3, Nothing, Just 8, Nothing, Nothing]
+    , [Nothing, Just 3, Nothing, Just 5, Nothing, Just 9, Nothing, Just 2, Nothing]
+    , [Just 7, Just 2, Nothing, Nothing, Nothing, Just 8, Nothing, Nothing, Just 4]
+    ]
 
-printSudoku :: Sudoku -> IO ()
-printSudoku [] = return ()
-printSudoku (row:rows) = do
-  putStrLn (concatMap show row)
-  printSudoku rows
+printSudoku :: Sudoku -> IO () -- Print the Sudoku grid
+printSudoku = mapM_ (putStrLn . concatMap showRow)
+
+showRow :: Maybe Int -> String -- Show a row in the Sudoku grid
+showRow Nothing  = " ."
+showRow (Just n) = ' ' : show n
+
+
